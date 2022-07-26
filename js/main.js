5be758f9f29720d25e106a0e3366cf5c3433cf2c
taku.kitano <kitaku95026@gmail.com>
2022-07-27 07:17:20 +0900

    home完成

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$.scrollify({
+  section: ".box",//1ページスクロールさせたいエリアクラス名
+  scrollbars: "false",//スクロールバー表示・非表示設定
+
+  easing: "swing", // 他にもlinearやeaseOutExpoといったjQueryのeasing指定可能
+  scrollSpeed: 200, // スクロール時の速度
+});
+
