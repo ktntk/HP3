ccfab10b12b0c1d85f037927c6659e3f2fdca8c5
taku.kitano <kitaku95026@gmail.com>
2022-07-30 14:28:28 +0900

    コーディング完了

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,9 @@
 // スライド並列表示==========================================================
 $('.slider').slick({
-  autoplay: false,
+  autoplay: true,
   infinite: false,
   slidesToShow: 4,
-  slidesToScroll: 2,
+  slidesToScroll: 4,
   prevArrow: '<div class="slick-prev"></div>',
   nextArrow: '<div class="slick-next"></div>',
   dots: true,
